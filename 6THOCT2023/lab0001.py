ad3ec61ce2254bf5a0c38a128ceffa380088981e
Akhil Wilson <128221970+Sahillwilson@users.noreply.github.com>
2023-10-08 00:34:44 +0530

    Add files via upload

---
+++ 6THOCT2023/lab0001.py
@@ -0,0 +1,13 @@
+  #calculator
+num1 = input("Enter the first number\n")
+num1 = (float(num1))
+
+num2 = input("Enter your second number:\n")
+num2 =(float(num2))
+
+
+print(type(num1))
+print("After addition",num1+num2,sep="=",end="\n")
+print("After Substraction",num2-num1,sep="=",end="\n")
+print("After multiplication",num1*num2,sep="=",end="\n")
+print("After Divison",num1/num2,sep="=",end="\n")
